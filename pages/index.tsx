0b75cc10c5f56fcf61c9b365f59eea894df8206b
William Huang <william.huang001@binus.ac.id>
2023-03-07 01:09:48 +0700

    change home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { InferGetStaticPropsType, NextPage } from 'next';
+import BlogCard from '../components/BlogCard';
+import { readPostInfo } from '../lib/helper';
+import { PostApiResponse } from '../utils/types';
 
-const Home: NextPage = () => {
-  return (
-    <div>
 
-    </div>
-  )
-}
 
-export default Home
+export const getStaticProps = async () => {
+  // const { postInfo }: PostApiResponse = await fetch('http://localhost:3000/api/posts').then(data => data.json());
+
+  const postInfo: PostApiResponse = readPostInfo()
+  return {
+    props: { posts: postInfo }
+  }
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const Blogs: NextPage<Props> = ({ posts }) => {
+
+  return <div className='p-5 max-w-3xl mx-auto space-y-5'>
+    {posts.map(post => (
+      <BlogCard key={post.slug} title={post.title} desc={post.meta} slug={post.slug} />
+    ))}
+  </div>;
+
+};
+
+export default Blogs;
